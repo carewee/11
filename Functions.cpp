a6556a4e6c4f5468630a1029d64ddbdba852c6fc
carewee <carewee@gmail.com>
2015-04-24 13:43:15 -0400

    editing

--- Functions.cpp
+++ Functions.cpp
@@ -6,23 +6,27 @@
 
 using std::cout; using std::cin; using std::endl;
 
-void output(int *arrayPtr, int size) {
+void varArray(){
+    // initialization
+}
+
+void varArray::output() {
     // prints the values in "arrayPtr" of "size"
     
     for (int i = 0; i < size; ++i){
-        cout << arrayPtr[i] << " " ;
+        cout << dArray[i] << " " ;
     }
     
 }
 
 
-int varArray::check(int *arrayPtr, int number, int size){
+int varArray::check(int number){
     // returns the index of the element in "arrayPtr" of "size"
     // that corresponds to the element holding "number"
     // if number is not in the array, returns -1
     
     for (int i = 0; i < size; ++i) {
-        if (arrayPtr[i] == number)
+        if (dArray[i] == number)
             return i;
     }
     return -1;
@@ -30,32 +34,32 @@ int varArray::check(int *arrayPtr, int number, int size){
 
 
 
-void varArray::addNumber(int *& arrayPtr, int number, int &size){
+void varArray::addNumber(int number){
     // adds "number" to the array pointed to by "arrayPtr" of "size".
     // if the number is not already there, if "number" is there - no action
     // Note, the size of the array is thus increased.
     
     int *tmp = new int[size + 1];
     
-    if (check(arrayPtr, number, size) == -1) {
+    if (check(number) == -1) {
         for (int i = 0; i < size; ++i) {
-            tmp[i] = arrayPtr[i];
+            tmp[i] = dArray[i];
         }
         
     tmp[size] = number;
-    delete [] arrayPtr;
-    arrayPtr = tmp;
+    delete [] dArray;
+    dArray = tmp;
     size++;
     }
 
 }
 
-void varArray::removeNumber(int *& arrayPtr, int number, int &size){
+void varArray::removeNumber(int number){
     // removes a "number" from the "arrayPtr" of "size".
     // if "number" is not there -- no action
     // note, "size" changes
     
-    int index = check(arrayPtr, number, size);
+    int index = check(number);
     int *tmp = new int[size - 1];
     
     if (index == -1)
@@ -63,13 +67,13 @@ void varArray::removeNumber(int *& arrayPtr, int number, int &size){
     
     for (int i = 0; i < size; ++i) {
         if (i < index)
-            tmp[i] = arrayPtr[i];
+            tmp[i] = dArray[i];
 
         else
-            tmp[i] = arrayPtr[i + 1];
+            tmp[i] = dArray[i + 1];
     }
 
-    delete [] arrayPtr;
-    arrayPtr = tmp;
+    delete [] dArray;
+    dArray = tmp;
     size--;
 }
